126237a80c5199c8895565f2101afef7035133d9
Amjedusman <amjadusman98@gmail.com>
2024-10-25 10:36:15 +0530

    faculty issue solved

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+        domains:["gecskp.etlab.in"],
         remotePatterns: [
           {
             protocol: 'https',
